72a906d78554df41babc8cb569161176ae78f528
seung <seunggyu.im@gmail.com>
2020-01-04 22:10:12 +0900

    first commit

--- day2first.py
+++ day2first.py
@@ -3,42 +3,28 @@
 with open("input2.txt", "r") as f:
 
 
-# f.read reads one single character at a time, while split will return a splited list
-
   for x in f :
     number = x.split(",")
- # change numbers
- # run program
+    number = [int(string) for string in number]
+    number[1]=12
+    number[2]=2
+    x = len(number)
+    i=0
 
-  # python's for is foreach loop, there's no direct counter
-  # BUT! you can use, array and it's array methods to manipulate the list
-
-number = [int(string) for string in number]
-number[1]=12
-number[2]=2
-
-#print(number)
-#print(len(number))
-x = len(number)
-i=0
 while i<range(x) :
-    #print(i)
+ 
     if number[i] == 1 :
         a = number[i+3]
         b = number[i+1]
         c = number[i+2]
         number[a] = number[b]+number[c]
         i +=4
-        print(i)
-        #print("when number is 1")
+               
     elif number[i] == 2 :
        a = number[i+3]
-       #print(a)
        b = number[i+1]
        c = number[i+2]
-       #print(number[a])
-       #print(number[b])
-       #print(number[c])
+      
        number[a] = (number[b])*(number[c])
        i +=4
     elif number[i] == 99 :
@@ -49,7 +35,5 @@ while i<range(x) :
 
 
 
-
 print(number)
 
-# consider the case when 1, 2 is used as noneopcode
--- day2second.py
+++ day2second.py
@@ -1,13 +1,37 @@
-def recursivesum(x):
-   sum=0
-   if x <=0:
-      return 0 #break out of loop
-   else :
-      return x+recursivesum(x//3-2)
+import copy 
+
+with open("input2.txt", "r") as f:
+    
+    for a in f:
+        number = a.split(",")
+        og_number = [int(string) for string in number]
+	
+        for x, y in ((a, b) for a in range(100) for b in range(100)):
+            number = og_number
+            number[1] = x
+            number[2] = y
+            i = 0
+            z = len(number)
+            while i < z:
+                if number[i] == 1:
+                    a = number[i + 3]
+                    b = number[i + 1]
+                    c = number[i + 2]
+                    number[a] = number[b] + number[c]
+                    i += 4
+                elif number[i] == 2:
+                    a = number[i + 3]
+                    b = number[i + 1]
+                    c = number[i + 2]
+                    number[a] = (number[b]) * (number[c])
+                    i += 4
+                elif number[i] == 99:
+                    break
+            if number[0] == 19690720:
+                print("found it", 100*x+y)
+
+                break
+
+print(number)
 
 
-f = open("input.txt", "r")
-fuel=0;
-for x in f:
-   fuel += recursivesum(int(x)//3-2)
-print fuel
